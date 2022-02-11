56ac8d6e001a1df010c342f4a31ff67310df4ee3
Jameson Lajoie <jlajoie2@jhu.edu>
2022-02-11 12:44:41 -0500

    Worked on comments for entire code file of arithmetic.c

--- homeworks/hw1/arithmetic.c
+++ homeworks/hw1/arithmetic.c
@@ -3,40 +3,23 @@
 
 #include<stdio.h>
 
+// The main section of the code arithmetic.c to take a user inputted arithmetic and output the result.
 int main(void) {
   printf("Please enter an arithmetic expression using * and / only:\n");
   
-
+  //declare var
   char op;
   float num;
   float result;
   int scanner;
+  // check first char input and test its validity
   if (scanf(" %f", &result) != 1) {
     printf("malformed expression\n");
     return 1;
   }
-    
+  //while loop to continue taking input from the user
   while ((scanner = (scanf(" %c %f",&op, &num))) == 2) {
 
-    // switch(op) {
-
-    // case '/':
-    /* if (num == 0){
-        printf("division by zero\n");
-        break;
-      }
-      else {
-      result = result / num;
-      }
-      break;
-
-    case '*':
-      result = result * num;
-      break;
-
-    default:
-      printf("malfor
-      }*/
     if (op == '/') {
       //check for division by 0
       if (num == 0){
@@ -54,10 +37,12 @@ int main(void) {
     }
     
     }
+  //check if input from the user that did not pass through the while loop is valid
     if (scanner != 2 && scanner != EOF) {
       printf("malformed expression\n");
       return 1;
     }
+    //print results  
     printf("%f\n",result);
     return 0;
 }
